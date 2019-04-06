8ae9c69c5c9cd7cb63a5efb5d9786f41b3aaf95f
HuiDong Wu <wuhd@cn.ibm.com>
2019-04-06 23:23:17 +0800

    edit

--- exapmle/main.data.js
+++ exapmle/main.data.js
@@ -1,25 +1,31 @@
 module.exports.maintestdata = [
   {
     description: '测试用例1',
+    // 定义被测函数传入参数
     input: [
       1,
       2,
     ],
+    // 定义被测函数输出
     output: {
       isError: false,
       returnvalue: 4,
     },
+    // 定义被mock函数状态
     submock: {
+      // 被mock的函数预计是否会被调用
       isCalled: true,
+      // 被mock的函数被调用时的参数
       expectParam: [
         1,
         2,
       ],
+      // 定义被mock函数的mock实现
       mockasfunc: () => 3,
     }
   },
   {
-    description: '测试用例1 异常',
+    description: '测试用例2 异常',
     input: [
       1,
       2,
